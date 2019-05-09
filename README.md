afcca62ce41c2dd10a336a432f841fce3812fc76
Rune Kaagaard <rumi.kg@gmail.com>
2019-05-09 12:15:47 +0200

    Nogil version looks good.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # python-htmlgen
-Fast (hopefully), pure python, caching and diffing html generator.
+Fast (hopefully), pure python, parallizable, caching and diffing html generator. No more templates, just write python (or cython).
+
+# High level Python api:
+
+    @hgen
+    def my_page(sections):
+        h1("My Page", _class="top-h1")
+        for section in sections:
+            with section_cm(height=100):
+                div(section.content, style={"margin": auto})
+
+# Caching context manager
+
+    @hgen
+    def my_page(
+        for section in sections:
+            with section_cm(height=100), cache_cm(key=my_page, section=section) as data:
+                div(data.content, style={"margin": auto})
+                
+# Low level Python api
+
+    @hgen
+    def my_page(items):
+        for item in items:
+            o_div(_class="item") # Opens.
+            write(item.text) # Writes verbatim.
+            c_div() # Closes.
+        
+# Pure c Cython (nogil)
+
+Does not touch python.
+
+    cdef string page_cython_nogil(int n) nogil:
+        htmlgen_start()
+        cdef char i_str[10]
+
+        o_ul_ng()
+        for i in range(n):
+            sprintf(i_str, <char*> "%d", k)
+
+            div_ng(<char*> "My content.", [
+                a(<char*> "class", <char*> "a-class"),
+                a(<char*> "width", <char*> i_str),
+                T # Terminates.
+            ])
+        c_ul_ng()
+                
+        return htmlgen_stop()
--- cython_proof_of_concept.pyx
+++ cython_proof_of_concept.pyx
@@ -3,9 +3,7 @@
 
 
 from cymem.cymem cimport Pool
-from random import random
     
-import threading
 from contextlib import contextmanager
 
 from cpython cimport array
@@ -13,35 +11,58 @@ from cython.parallel cimport parallel
 from cython.parallel import prange
 cimport openmp
 from cymem.cymem cimport Pool
-import multiprocessing
 from libcpp.string cimport string
 from libcpp.vector cimport vector
-from libc.stdio cimport snprintf
+from libc.stdio cimport sprintf
 
-from pyrsistent import freeze
+### Type definitions ###
+ctypedef fused attr_val:
+    char
+    string
+    float
 
+cdef struct attr_s:
+    string name
+    string value
+
+ctypedef attr_s attr
+
+# Creates an attr.
+cdef attr a(string name, attr_val value) nogil:
+    cdef attr _a
+    #cdef char* value_c = value
+    
+    _a.name = <string> name
+    _a.value = <string> value
+
+    return _a
+
+# Attribute list terminator.
+cdef attr T = a(<char*> "__the_end__", <char*> "__is_reached__")
+
+
+### Cdefs ###
 cdef:
     struct Thread:
-        int index
         string html
-    int n_threads = multiprocessing.cpu_count()
+    int n_threads = openmp.omp_get_max_threads()
     Pool mem = Pool()
     Thread* threads = <Thread*>mem.alloc(n_threads, sizeof(Thread))
-    string e1 = '<'
-    string e2 = ' class="'
-    string e3 = '">'
-    string e4 = "</"
-    string e5 = ">"
-    string e6 = '<'
-    string e7 = '<'
-    string e8 = "div"
+    char* e1 = '<'
+    char* e2 = ' class="'
+    char* e3 = '>'
+    char* e4 = "</"
+    char* e5 = ">"
+    char* e6 = '="'
+    char* e7 = ' '
+    char* e8 = "div"
+    char* e9 = '"'
 
 
 cdef void htmlgen_start() nogil:
     cdef int i = openmp.omp_get_thread_num()
     threads[i].html.append("-")
     threads[i].html.clear()
-    threads[i].index = 0
 
 cdef string htmlgen_stop() nogil:
     cdef int i = openmp.omp_get_thread_num()
@@ -50,27 +71,35 @@ cdef string htmlgen_stop() nogil:
 cdef void write(string html) nogil:
     cdef int i = openmp.omp_get_thread_num()
     threads[i].html.append(html)
-    threads[i].index += 1
 
-cdef void _element_open(string tag, string class_) nogil:
-    write(e1)
-    write(tag)
-    write(e2)
-    write(class_)
-    write(e3)
+cdef void _element_open(string tag, attr* attrs) nogil:
+    cdef:
+        int i = openmp.omp_get_thread_num()
+        int j = 0
+        
+    threads[i].html.append(e1).append(tag)
+    
+    while True:
+        a = attrs[j]
+        if a.name == T.name:
+            break
+        
+        threads[i].html.append(e7).append(a.name).append(e6).append(a.value
+            ).append(e9)
+        j = j + 1
+        
+    threads[i].html.append(e3)
 
 cdef void _element_close(string tag) nogil:
-    write(e4)
-    write(tag)
-    write(e5) 
+    threads[openmp.omp_get_thread_num()].html.append(e4).append(tag).append(e5)
 
-cdef void element(string tag, string inner, string class_) nogil:
-    _element_open(tag, class_)
-    write(inner)
+cdef void element(string tag, string inner, attr* attrs) nogil:
+    _element_open(tag, attrs)
+    threads[openmp.omp_get_thread_num()].html.append(inner)
     _element_close(tag)
 
-cdef void div(string inner, string class_) nogil:
-     element(e8, inner, class_)
+cdef void div(string inner, attr* attrs) nogil:
+    element(e8, inner, attrs)
 
 # -------------------------
 
@@ -95,66 +124,91 @@ cdef string pelement(string tag, string inner, string class_) nogil:
 cdef string pdiv(string inner, string class_) nogil:
     return pelement(e8, inner, class_)
 
-@contextmanager
-def divcm(string class_):
-    cdef int i = openmp.omp_get_thread_num()
-    cdef int index = threads[i].index
-    _element_open(<string> "div", class_)
-    yield
-    _element_close(<string> "div")
+# @contextmanager
+# def divcm(string class_):
+#     cdef int i = openmp.omp_get_thread_num()
+#     cdef int index = threads[i].index
+#     _element_open(<string> "div", class_)
+#     yield
+#     _element_close(<string> "div")
 
 
-cdef int N = 1000
-cdef int M = 1000
+cdef int N = 1
+cdef int M = 5
 
-cdef string s = "My li is 500"
-cdef string e = "Foo"
+cdef char* s = "My li is ø 500"
+cdef char* e = "Foøo"
 
-cdef string page_cython(int n, int m):
-    htmlgen_start()
-    cdef int j = 0
+# cdef string page_cython(int n, int m):
+#     htmlgen_start()
+#     cdef int j = 0
 
-    with divcm("the-class"):
-        div("My things", "Foo")
-        while j < n:
-            for k in range(m):
-                div("My li is {}".format(j), "Foo")
-            j += 1
+#     with divcm("the-class"):
+#         div("My things", "Foo")
+#         while j < n:
+#             for k in range(m):
+#                 div("My li is {}".format(j), "Foo")
+#             j += 1
 
-    return htmlgen_stop()
-            
+#     return htmlgen_stop()
+
+cdef void wat(string x) nogil:
+    cdef string y = <string> x
+
 cdef string page_cython_nogil(int n, int m) nogil:
+    #with gil:
+    #    cdef ctuple attrs = (<string> "class", <string> "my-class") 
     htmlgen_start()
     cdef int j = 0
+    cdef char k_str[10]
 
     while j < n:
         for k in range(m):
-            div(s, e)
+            sprintf(k_str, <char*> "%d", k)
+            
+            div(<char*> "This is gøød", [
+                a(<char*> "height", <char*> "91"),
+                a(<char*> "width", <char*> k_str),
+                T
+            ])
+            div(<char*> "Classical", [
+                a(<char*> "class", <char*> "it-is"),
+                a(<char*> "title", <char*> "My øwesome title"),
+                T
+            ])
         j += 1
 
     return htmlgen_stop()
 
-cdef string page_cython_parallel(int n, int m):
-    htmlgen_start()
-    
-    cdef:
-        int j = 0
-        int k = 0
-        int size = n * m
-        int l = 0
-        string* parts = <string*>mem.alloc(size, sizeof(string))
+# cdef string wut(string, (int, int) attrs) nogil:
+#     cdef char* x = "ok"
+#     wat(<char*> "WUUUTæ")
+#     wat(e)
+#     #print html, attrs
+#     #cdef string html_s = <string> html
+#     pass
 
-    for j in prange(n, nogil=True):
-        k = 0
-        while k < m:
-            l = j*n + k
-            parts[l] = pdiv(s, e)
-            k = k + 1
-
-    for part in parts[:size]:
-        write(part)
+# cdef string page_cython_parallel(int n, int m):
+#     htmlgen_start()
     
-    return htmlgen_stop()
+#     cdef:
+#         int j = 0
+#         int k = 0
+#         int size = n * m
+#         int l = 0
+#         string* parts = <string*>mem.alloc(size, sizeof(string))
+
+#     for j in prange(n, nogil=True):
+#         k = 0
+#         while k < m:
+#             l = j*n + k
+#             parts[l] = pdiv(s, e)
+#             k = k + 1
+
+#     for part in parts[:size]:
+#         write(part)
+    
+#     return htmlgen_stop()
 
 import time
 def timer(name, func):
@@ -167,25 +221,25 @@ def timer(name, func):
     #print func()
     b = time.time() * 1000
     took = b - a
-    print
-    print "##############################################################"
-    print name, N*M,"items took", round(took), "Milliseconds"
-    print "each item took", took / float(N*M) * 1000, "u seconds"
-    print
+    print()
+    print( "##############################################################")
+    print( name, N*M,"items took", round(took), "Milliseconds")
+    print( "each item took", took / float(N*M) * 1000, "u seconds")
+    print()
     if output is not None:
-        print output
+        print( output)
     
     return took
 
-a = timer("Page cython", page_cython)
+#a1 = timer("Page cython", page_cython)
 d = timer("Page cython no gil", page_cython_nogil)
-c = timer("Page cython parallel", page_cython_parallel)
+#c = timer("Page cython parallel", page_cython_parallel)
     
 from proof_of_concept import page_pure_python
-b = timer("Page pure python", page_pure_python)
+#b = timer("Page pure python", page_pure_python)
 
-print "\n----------------------------"
-print "Speedup = ", b / float(d)
+print ("\n----------------------------")
+#print "Speedup = ", b / float(d)
 
 #print page()
 
