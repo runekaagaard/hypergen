31f2918ae7600f5a8365e848d1f02593b9c8186c
Rune Kaagaard <rumi.kg@gmail.com>
2019-05-12 19:08:29 +0200

    Adding cache context manager back again.

--- src/hypergen.py
+++ src/hypergen.py
@@ -11,7 +11,6 @@ if sys.version_info.major > 2:
 
     def t(s, quote=True):
         return escape(str(s), quote=quote)
-
 else:
     from cgi import escape
 
@@ -22,18 +21,20 @@ else:
         return escape(unicode(s), quote=quote)
 
 
-data = local()
+state = local()
 
 
 def hypergen(func, *args, **kwargs):
     try:
-        data.html = []
-        data.extend = data.html.extend
+        state.html = []
+        state.extend = state.html.extend
+        state.cache_client = kwargs.pop("cache_client", None)
         func(*args, **kwargs)
-        html = u"".join(data.html)
+        html = u"".join(state.html)
     finally:
-        data.html = []
-        data.extend = None
+        state.html = []
+        state.extend = None
+        state.cache_client = None
 
     return html
 
@@ -55,7 +56,7 @@ def tag_open(tag, *inners, **attrs):
                 (k, attrs["style"][k]) for k in sorted(attrs["style"].keys()))
 
     sep = attrs.pop("sep", u"")
-    e = data.extend
+    e = state.extend
     e((u"<", tag))
     for k, v in items(attrs):
         k = t(k).lstrip("_").replace("_", "-")
@@ -73,12 +74,49 @@ def tag_open(tag, *inners, **attrs):
 
 def tag_close(tag, *inners, **kwargs):
     write(*inners, **kwargs)
-    data.extend((u"</", t(tag), u">"))
+    state.extend((u"</", t(tag), u">"))
 
 
 def write(*inners, **kwargs):
     sep = kwargs.pop("sep", u"")
-    data.extend((t(sep).join(t(inner) for inner in inners), ))
+    state.extend((t(sep).join(t(inner) for inner in inners), ))
+
+
+class Bunch(dict):
+    def __getattr__(self, k):
+        return self[k]
+
+
+class SkipException(Exception):
+    pass
+
+
+@contextmanager
+def skippable():
+    try:
+        yield
+    except SkipException:
+        pass
+
+
+@contextmanager
+def cached(key=None, **kwargs):
+    keys = key if type(key) in (list, tuple) else [key]
+    hash_value = hash(
+        tuple([hash(tuple(keys))] + ["SEP_A"] + [(k, v)
+                                                 for k, v in items(kwargs)]))
+    client = state.cache_client
+    html = client.get(hash_value)
+
+    if html is not None:
+        state.extend((html, ))
+        raise SkipException()
+    else:
+        a = len(state.html)
+        kwargs.update({"key": key, 'hash': hash_value})
+        yield Bunch(kwargs)
+        b = len(state.html)
+        client.set(hash_value, u"".join(x for x in state.html[a:b]))
 
 
 ### *div* functions. ###
@@ -105,29 +143,60 @@ def c_div(*inners, **kwargs):
 
 if __name__ == "__main__":
     # yapf: disable
-    def test1():
-        div("Hello, world!")
-    assert hypergen(test1) == u"<div>Hello, world!</div>"
-
-    def test2(name):
-        div("Hello", name, _class="its-hyper", data_x=3.14, hidden=True,
-            selected=False, style={"height": 42, "display": "none"}, sep=" ",
-            _sort_attrs=True)
-    assert hypergen(test2, "hypergen!") == u'<div class="its-hyper" '\
-        'data-x="3.14" hidden style="display:none;height:42">'\
-        'Hello hypergen!</div>'
-
-    def test3():
-        with div_cm("div", "cm", x=1, sep="_"):
-            o_div(1, 2, y=1, sep="-")
-            write(3, 4, sep="+")
-            c_div(5, 6, sep=" ")
-    assert hypergen(test3) == u'<div x="1">div_cm<div y="1">1-23+45 6</div>'\
-        '</div>'
-
-    def test4():
+    def test_basics():
         tag_open("li", 1, 2, a=3, _b=4, sep=".", style={1: 2}, x=True, y=False,
                  _sort_attrs=True)
         write(5, 6, sep=",")
         tag_close("li", 7, 8, sep="+")
-    assert hypergen(test4) == u'<li b="4" a="3" style="1:2" x>1.25,67+8</li>'
+    assert hypergen(test_basics) == u'<li b="4" a="3" style="1:2" x>1.25,67+8</li>'
+
+    class Cache(object):
+        def __init__(self):
+            self.cache = {}
+
+        def set(self, k, v):
+            self.cache[k] = v
+
+        def get(self, k):
+            return self.cache.get(k, None)
+    cache_client = Cache()
+
+    _h = None
+    _t = False
+    def test_cache(a, b):
+        global _h, _t
+        _t = False
+        with skippable(), cached(key=test_cache, a=a, b=b) as value:
+            div(*(value.a+value.b), data_hash=value.hash)
+            _h = value.hash
+            _t = True
+
+    assert hypergen(test_cache, (1, 2), (3, 4), cache_client=cache_client)\
+        == u'<div data-hash="{}">1234</div>'.format(_h)
+    assert _t is True
+    assert hypergen(test_cache, (1, 2), (3, 4), cache_client=cache_client)\
+        == u'<div data-hash="{}">1234</div>'.format(_h)
+    assert _t is False
+    assert hypergen(test_cache, (1, 2), (3, 5), cache_client=cache_client)\
+        == u'<div data-hash="{}">1235</div>'.format(_h)
+    assert _t is True
+
+
+    def test_div1():
+        div("Hello, world!")
+    assert hypergen(test_div1) == u"<div>Hello, world!</div>"
+
+    def test_div2(name):
+        div("Hello", name, _class="its-hyper", data_x=3.14, hidden=True,
+            selected=False, style={"height": 42, "display": "none"}, sep=" ",
+            _sort_attrs=True)
+    assert hypergen(test_div2, "hypergen!") == u'<div class="its-hyper" '\
+        'data-x="3.14" hidden style="display:none;height:42">'\
+        'Hello hypergen!</div>'
+
+    def test_div3():
+        with div_cm("div", "cm", x=1, sep="_"):
+            o_div(1, 2, y=1, sep="-")
+            c_div(5, 6, sep=" ")
+    assert hypergen(test_div3) == u'<div x="1">div_cm<div y="1">1-25 6</div>'\
+        '</div>'
