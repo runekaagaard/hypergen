18d678e1919d529ae4b991ee51b9a13e52ad0b6c
Rune Kaagaard <rumi.kg@gmail.com>
2019-05-12 16:56:08 +0200

    Python 2+3 support.

--- src/hypergen.py
+++ src/hypergen.py
@@ -1,17 +1,28 @@
 from threading import local
 from contextlib import contextmanager
 from collections import OrderedDict
+import sys
 
-try:
+if sys.version_info.major > 2:
     from html import escape
-except:
+
+    def items(x):
+        return x.items()
+
+    def t(s, quote=True):
+        return escape(str(s), quote=quote)
+
+else:
     from cgi import escape
 
+    def items(x):
+        return x.iteritems()
+
+    def t(s, quote=True):
+        return escape(unicode(s), quote=quote)
+
+
 data = local()
-
-
-def t(s, quote=True):
-    return escape(unicode(s), quote=quote)
 
 
 def hypergen(func, *args, **kwargs):
@@ -38,20 +49,22 @@ def tag_open(tag, *inners, **attrs):
     # For testing only, subject to change.
     sort_attrs = attrs.pop("_sort_attrs", False)
     if sort_attrs:
-        attrs = OrderedDict((k, attrs[k])
-                            for k in sorted(attrs, key=lambda x: x))
+        attrs = OrderedDict((k, attrs[k]) for k in sorted(attrs.keys()))
+        if u"style" in attrs and type(attrs["style"]) is dict:
+            attrs["style"] = OrderedDict(
+                (k, attrs["style"][k]) for k in sorted(attrs["style"].keys()))
 
     sep = attrs.pop("sep", u"")
     e = data.extend
     e((u"<", tag))
-    for k, v in attrs.iteritems():
+    for k, v in items(attrs):
         k = t(k).lstrip("_").replace("_", "-")
         if type(v) is bool:
             if v is True:
                 e((u" ", k))
-        elif k == u"style" and type(v) is dict:
+        elif k == u"style" and type(v) in (dict, OrderedDict):
             e((u" ", k, u'="', u";".join(
-                t(k1) + u":" + t(v1) for k1, v1 in v.iteritems()), u'"'))
+                t(k1) + u":" + t(v1) for k1, v1 in items(v)), u'"'))
         else:
             e((u" ", k, u'="', t(v), u'"'))
     e((u'>', ))
@@ -104,7 +117,6 @@ if __name__ == "__main__":
         'data-x="3.14" hidden style="display:none;height:42">'\
         'Hello hypergen!</div>'
 
-
     def test3():
         with div_cm("div", "cm", x=1, sep="_"):
             o_div(1, 2, y=1, sep="-")
@@ -114,8 +126,8 @@ if __name__ == "__main__":
         '</div>'
 
     def test4():
-        tag_open("li", 1, 2, a=3, _b=4, sep=".", style={1: 2}, x=True, y=False)
-        write(5, 6, sep=",", _sort_attrs=True)
+        tag_open("li", 1, 2, a=3, _b=4, sep=".", style={1: 2}, x=True, y=False,
+                 _sort_attrs=True)
+        write(5, 6, sep=",")
         tag_close("li", 7, 8, sep="+")
-    print hypergen(test4)
-    assert hypergen(test4) == u'<li a="3" style="1:2" x b="4">1.25,67+8</li>'
+    assert hypergen(test4) == u'<li b="4" a="3" style="1:2" x>1.25,67+8</li>'
