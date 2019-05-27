11fd465d368a95396eff62d54e3d1d9801fab41e
Rune Kaagaard <rumi.kg@gmail.com>
2019-05-27 20:35:46 +0200

    Id attribute and dataformats.

--- experiments/base6x_iter.py
+++ experiments/base6x_iter.py
@@ -2,24 +2,22 @@ import string
 
 
 def base65_counter():
-    BS = string.letters + string.digits + "-_:"
-    b = len(BS)
+    # THX: https://stackoverflow.com/a/49710563/164449
     i = -1
     while True:
         i += 1
-        s = i
-        res = ""
-        while s:
-            res += BS[s % b]
-            s //= b
-        res = res[::-1] or "a"
-        try:
-            int(res[0])
-            yield "h" + res
-        except ValueError:
-            yield res
+        num = i
+        abc = string.letters + string.digits + "-_:"
+        base = len(abc)
+        output = abc[num % base]  # rightmost digit
+
+        while num >= base:
+            num //= base  # move to next digit to the left
+            output = abc[num % base] + output  # this digit
+
+        yield output
 
 
 c = base65_counter()
-for i in range(100):
+for i in range(500):
     print next(c),
