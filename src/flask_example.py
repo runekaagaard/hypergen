c2d6ffe1eec0dd36e6153531329e92d05bbbc306
Rune Kaagaard <rumi.kg@gmail.com>
2019-06-10 21:57:53 +0200

    Starting new example.

--- src/flask_example.py
+++ src/flask_example.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from __future__ import (absolute_import, division, unicode_literals)
 # To run, pip install flask, and then
 #     FLASK_ENV=development FLASK_APP=flask_example flask run
 
@@ -7,7 +9,9 @@ from flask import Flask, url_for
 from hypergen import (flask_liveview_hypergen as hypergen,
                       flask_liveview_callback_route as callback_route, div,
                       input_, script, raw, label, p, h1, ul, li, a, html, head,
-                      body, link, table, tr, th, td, THIS, pre)
+                      body, link, table, tr, th, td, THIS, pre, section, ol,
+                      write, span, button)
+from random import randint
 
 NORMALISE = "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
 SAKURA = "https://unpkg.com/sakura.css/css/sakura.css"
@@ -44,7 +48,8 @@ def index():
         h1("Browse the following examples")
         ul(
             li.r(a.r("Basic counter", href=url_for("counter"))),
-            li.r(a.r("Input fields", href=url_for("inputs"))), )
+            li.r(a.r("Input fields", href=url_for("inputs"))),
+            li.r(a.r("Petals around the rose", href=url_for("petals"))), )
 
     return hypergen(base_template, template)
 
@@ -115,13 +120,60 @@ def inputs():
 
 ### Petals around the rose ###
 PETALS = {3: 2, 5: 4}
-RULES = """
-There are three rules:
-    1) The name of the game is "Petals Around the Rose, 
-       and the name of the game is the key to the game.
-    2) The answer is always zero or an even number.
-    3) Anyone who knows the game may give the answer to 
-       any roll, but they must not disclose the reasoning.
+DIES = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+R1 = ("The name of the game is Petals Around the Rose, and the name of the "
+      "game is the key to the game.")
+R2 = "The answer is always zero or an even number."
+R3 = ("Anyone who knows the game may give the answer to any roll, but they "
+      "must not disclose the reasoning.")
+RULES = section.r(
+    p.r("There are three rules:", ol.r(li.r(x) for x in [R1, R2, R3])),
+    p.r("Get six correct answers in a row to become a Potentate of the Rose."))
+QUESTIONS = []
+ANSWERS = []
 
-Get six correct answers in a row to become a Potentate of the Rose.
-"""
+
+def petals_template():
+    write(RULES)
+
+    def question_template(question, show_answer=True):
+        p(
+            span.r(
+                (DIES[x - 1] for x in question),
+                sep=" ",
+                style={"font-size": "50px"}), )
+        with p.c():
+            label.r("Whats the answer?")
+            answer = input_(type_="number")
+            button("Submit", onclick=(petal_answer, answer))
+
+    question_template(QUESTIONS[-1], False)
+    with table.c():
+        tr(th.r(x.capitalize()) for x in ("throw", "answer", "correct answer"))
+        for question, answer in zip(reversed(QUESTIONS), reversed(ANSWERS)):
+            tr(
+                td.r(x)
+                for x in ((DIES[x - 1]
+                           for x in question), answer, result(question)))
+
+
+def result(question):
+    return sum(PETALS.get(x, 0) for x in question)
+
+
+def roll():
+    QUESTIONS.append([randint(1, 6) for _ in range(5)])
+
+
+@callback_route(app, '/petal-answer/')
+def petal_answer(answer):
+    ANSWERS.append(answer)
+    html = hypergen(base_template, petals_template, target_id="content")
+    roll()
+    return html
+
+
+@app.route('/petals/')
+def petals():
+    roll()
+    return hypergen(base_template, petals_template)
