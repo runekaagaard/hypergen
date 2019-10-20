8e2936007ad0eb3f88e1726a9432b081a382e109
Rune Kaagaard <rumi.kg@gmail.com>
2019-10-20 16:32:44 +0200

    Thinking about change.

--- experiments/change_aware_wrapper.py
+++ experiments/change_aware_wrapper.py
@@ -12,7 +12,7 @@ class ChangeAware(object):
             setattr(self.path[-1], k, v)
 
     def __setitem__(self, k, v):
-        print "Setting", k, v
+        print("Setting", k, v)
         self.path[-1][k] = v
 
     def __getattr__(self, k):
@@ -25,10 +25,38 @@ class X(object):
 
 data = ChangeAware(dict(x=X()))
 data.x.y = 1234
-print data.x.y
+print(data.x.y)
 
 #data.foo = 42
 #data[3] = 91
 #setattr(data, 3, 91)
+#print data.foo.changed()
 
-print data.foo.changed()
+
+class CA2(object):
+    def __init__(self, data):
+        self.data = data
+
+    def __getattribute__(self, k):
+        print "__getattribute__", k
+        if k == "data":
+            return self.__dict__["data"]
+        else:
+            v = object.__getattribute__(self, k)
+            if type(v) in (str, int, float, long):
+                return v
+            else:
+                return CA2(v)
+
+    def __getitem__(self, k):
+        print "__getitem__", k
+        return self.data[k]
+
+    def __setitem__(self, k, v):
+        print "__setitem__", k, v
+        self.data[k] = v
+
+
+ca = CA2({1: {2: 3}})
+ca[1][2] = 42
+print ca.data
