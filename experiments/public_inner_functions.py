4f92b281fdf2e0b2f97adc74e099249cf3c7a635
Rune Kaagaard <rumi.kg@gmail.com>
2021-01-20 20:52:15 +0100

    Experimenting with public inner functions.

---
+++ experiments/public_inner_functions.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+def ipub(outer):
+    """
+    Decorate an inner function to make it public to the outside.
+    """
+    def _(inner):
+        setattr(outer, inner.__name__, inner)
+        return inner
+
+    return _
+
+def a(x):
+    @ipub(a)
+    def b(x):
+        @ipub(b)
+        def c(x):
+            return x ** x
+
+        return x * x
+    
+    return x + x 
+    
+print a(4)
+print a.b(4)
+print a.b.c(4)
